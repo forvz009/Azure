e861e9775189fe898f5547ab923d80e96e0a99cf
Forexium <150202229+Forexium@users.noreply.github.com>
2025-03-04 17:34:28 -0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,11 @@
+getgenv().script_key = getgenv()['script_key']
+
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer or Players.PlayerAdded:Wait()
 
 local GamesTable = {
-    [6061766680] = "https://api.luarmor.net/files/v3/loaders/e3ebd4aadbd4e1d3b121f66b06102e8e.lua", -- Volleyball Legends (don't modify this you nerd)
-    [6931042565] = "https://api.luarmor.net/files/v3/loaders/f5ec84cb8ff1b58ef725bbcc589fac55.lua", -- FIAS (don't modify this you nerd)
+    [6061766680] = "https://api.luarmor.net/files/v3/loaders/e3ebd4aadbd4e1d3b121f66b06102e8e.lua", 
+    [6931042565] = "https://api.luarmor.net/files/v3/loaders/f5ec84cb8ff1b58ef725bbcc589fac55.lua",
 }
 
 local GameId = game.GameId
@@ -21,6 +23,8 @@ if GameId == 6061766680 and table.find(HorribleExploits, Executor) then
     LocalPlayer:Kick("The Executor you're using is not supported.")
 elseif Executor == "Fluxus" then
     LocalPlayer:Kick("The Executor you're using is horrible please don't use this.")
+elseif Executor == "Delta" then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8f819170363aa88419a80cc565b6a53c.lua"))()
 end
 
 loadstring(game:HttpGet(Link))()
